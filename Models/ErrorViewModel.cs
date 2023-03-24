3484997489fd41fe7c6e04f99b2145619f1b0da0
Zachary <ZackCipolletta@gmail.com>
2023-03-24 09:52:58 -0700

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PierresSweetandSavoryTreats.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
